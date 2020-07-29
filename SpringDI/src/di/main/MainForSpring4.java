f5ada0c1b78f7a79aeed744faf01d4d2394d16a6
zannew <63032810+zannew@users.noreply.github.com>
2020-07-29 20:13:23 +0900

    Spring DI - 0729

--- SpringDI/src/di/main/MainForSpring4.java
+++ SpringDI/src/di/main/MainForSpring4.java
@@ -22,7 +22,7 @@ public class MainForSpring4 {
 		MemberRegisterService2 mrService1 = ctx.getBean("memberregSvc", MemberRegisterService2.class);
 		MemberRegisterService2 mrService2 = ctx.getBean("memberregSvc", MemberRegisterService2.class);
 		
-		
+		// prototype으로 생성된 빈 객체 - 빈을 요청할 때마다 새롭게 생성한다.
 		boolean chk1 = mrService1==mrService2;
 		System.out.println("mrService1==mrService2 → "+chk1);
 		
@@ -30,7 +30,8 @@ public class MainForSpring4 {
 		
 		ChangePasswordService2 cpService1 = ctx.getBean("memberPwSvc", ChangePasswordService2.class);
 		ChangePasswordService2 cpService2 = ctx.getBean("memberPwSvc", ChangePasswordService2.class);
-		
+	
+		// singleton으로 생성된 빈 객체 - 컨테이너에 하나의 객체만 생성된다.
 		boolean chk2 = cpService1==cpService2;
 		System.out.println("cpService1==cpService2 → "+chk2);
 		
